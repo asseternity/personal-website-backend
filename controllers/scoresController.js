6d1932fdecadfc92c19deddcedd6fa65030b3122
asseternity <nakupovasset@gmail.com>
2025-04-19 20:21:40 +0400

    feat: init scoresController with getScores and postScore functions

--- controllers/scoresController.js
+++ controllers/scoresController.js
@@ -1,7 +1,38 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-const getScores = async (req, res, next) => {};
-const postScore = async (req, res, next) => {};
+const getScores = async (req, res, next) => {
+  try {
+    const scores = await prisma.score.findMany({
+      orderBy: {
+        score: "desc",
+      },
+      take: 10,
+    });
+    return res.status(200).json(scores);
+  } catch (error) {
+    return next(error);
+  }
+};
+const postScore = async (req, res, next) => {
+  try {
+    const { name, score } = req.body;
+    await prisma.score.create({
+      data: {
+        name,
+        score,
+      },
+    });
+    const scores = await prisma.score.findMany({
+      orderBy: {
+        score: "desc",
+      },
+      take: 10,
+    });
+    return res.status(200).json(scores);
+  } catch (error) {
+    return next(error);
+  }
+};
 
 module.exports = { getScores, postScore };
