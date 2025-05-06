b0324d16823669cb368afcea1307778f575bc59e
asseternity <nakupovasset@gmail.com>
2025-05-06 17:29:30 +0400

    feat: update postScore to update your score if you post another score

--- controllers/scoresController.js
+++ controllers/scoresController.js
@@ -17,12 +17,26 @@ const getScores = async (req, res, next) => {
 const postScore = async (req, res, next) => {
   try {
     const { username, score } = req.body;
-    await prisma.score.create({
-      data: {
-        username,
-        score,
-      },
+
+    const existing = await prisma.score.findUnique({
+      where: { username },
     });
+
+    if (existing) {
+      if (score > existing.score) {
+        await prisma.score.update({
+          where: { username },
+          data: { score },
+        });
+      }
+    } else {
+      await prisma.score.create({
+        data: {
+          username,
+          score,
+        },
+      });
+    }
     const scores = await prisma.score.findMany({
       orderBy: {
         score: "desc",
