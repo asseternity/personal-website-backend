94e1ef3b4123b7467c7503e3bf307d3d68b6a56e
asseternity <nakupovasset@gmail.com>
2025-05-09 17:34:17 +0400

    feat: separate postScore into two middleware to postNewScore and postCheckScore

--- controllers/scoresController.js
+++ controllers/scoresController.js
@@ -18,15 +18,22 @@ const getScores = async (req, res, next) => {
 const postCheckScore = async (req, res, next) => {
   const { score } = req.body;
   try {
+    // Grab the top 5 descending
     const topFive = await prisma.score.findMany({
       orderBy: { score: "desc" },
       take: 5,
     });
-    if (score > topFive[4].score || topFive.length < 5) {
+
+    // If we have fewer than 5 entries, it's automatically top-five
+    if (topFive.length < 5) {
       return res.status(200).json({ topFive: true });
-    } else {
-      return res.status(200).json({ topFive: false });
     }
+
+    // Otherwise compare to the 5th place score
+    const fifthPlaceScore = topFive[topFive.length - 1].score;
+    const isTopFive = score > fifthPlaceScore;
+
+    return res.status(200).json({ topFive: isTopFive });
   } catch (error) {
     return next(error);
   }
