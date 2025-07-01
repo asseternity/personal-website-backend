6b407d8fc82a5e7d41fe113c96118655f86e5a91
asseternity <nakupovasset@gmail.com>
2025-07-01 14:57:04 +0400

    fix: update directory in blogSync

--- helper-functions/blogSync.js
+++ helper-functions/blogSync.js
@@ -4,7 +4,7 @@ const matter = require("gray-matter");
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-const blogDirectory = path.join(__dirname, "blog");
+const blogDirectory = path.join(__dirname, "..", "blogPosts");
 
 const getBlogPostFiles = () => {
   return fs.readdirSync(blogDirectory).filter((file) => file.endsWith(".md"));
